e36be3cc0d658b5d3896b1b0226e25f264e6a6dc
Aayush Gupta <aayushgupta054@gmail.com>
2025-08-20 11:03:43 +0530

    Setup for the project and clerk middleware

---
+++ middleware.ts
@@ -0,0 +1,50 @@
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server';
+
+// lets everyone access these routes not only signed in users
+const isPublicRoute = createRouteMatcher([
+    "/signin",
+    "/signup",
+    "/",
+    "/home"
+])
+
+const isPublicApiRoute = createRouteMatcher([
+    "/api/videos"
+])
+
+
+export default clerkMiddleware(async (auth, req) => {
+  const {userId} = await auth();
+  const currentUrl = new URL(req.url)
+  const isAccessingDashboard = currentUrl.pathname === "/home"
+  const isApiRequest = currentUrl.pathname.startsWith("/api")
+
+  // if user is already signed in direct him to home
+  if(userId && isPublicRoute(req) && !isAccessingDashboard){
+    return NextResponse.redirect(new URL("/home", req.url))
+  }
+  // not logged in
+  if(!userId){
+    // trying to access protected page
+    if(!isPublicRoute(req) && !isPublicApiRoute(req)){
+        return NextResponse.redirect(new URL("/signin", req.url))
+    }
+
+    //trying to access protected api route
+    if(isApiRequest && !isPublicApiRoute(req)){
+        return NextResponse.redirect(new URL("/signin", req.url))
+    }
+  }
+})
+
+export const config = {
+    // matcher means it will execute the middleware on all these
+  
+    matcher: [
+    // Skip Next.js internals and all static files, unless found in search params
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
